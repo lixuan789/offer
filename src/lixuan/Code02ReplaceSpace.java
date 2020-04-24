838e2d75659897c88bd83b122bf11f671480a8fb
lixuan789 <1255470835@qq.com>
2020-04-24 10:40:56 +0800

    offer

--- src/lixuan/Code02ReplaceSpace.java
+++ src/lixuan/Code02ReplaceSpace.java
@@ -9,9 +9,9 @@ public class Code02ReplaceSpace {
         String s = str.toString();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i)==' '){
+            if (s.charAt(i) == ' ') {
                 sb.append("%20");
-            }else {
+            } else {
                 sb.append(s.charAt(i));
             }
         }
@@ -20,16 +20,51 @@ public class Code02ReplaceSpace {
 
     /**
      * 方法二：调用API
+     *
      * @param str
      * @return
      */
     public static String replaceSpace2(StringBuffer str) {
 
-        return str.toString().replace(" ","%20");
+        return str.toString().replace(" ", "%20");
+    }
+
+    /**
+     * ① 在字符串尾部填充任意字符，使得字符串的长度等于替换之后的长度。因为一个空格要替换成三个字符（%20），
+     * 所以当遍历到一个空格时，需要在尾部填充两个任意字符。
+     * <p>
+     * ② 令 P1 指向字符串原来的末尾位置，P2 指向字符串现在的末尾位置。P1 和 P2 从后向前遍历，
+     * 当 P1 遍历到一个空格时，就需要令 P2 指向的位置依次填充 02%（注意是逆序的），
+     * 否则就填充上 P1 指向字符的值。从后向前遍是为了在改变 P2 所指向的内容时，不会影响到 P1 遍历原来字符串的内容。
+     * <p>
+     * ③ 当 P2 遇到 P1 时（P2 <= P1），或者遍历结束（P1 < 0），退出。
+     *
+     * @param str
+     * @return
+     */
+    public static String replaceSpace3(StringBuffer str) {
+        int p1 = str.length() - 1;
+        for (int i = 0; i <= p1; i++) {
+            if (str.charAt(i) == ' ') {
+                str.append("  ");
+            }
+        }
+        int p2 = str.length() - 1;
+        while (p1 >= 0 && p2 > p1) {
+            char c = str.charAt(p1--);
+            if (c == ' ') {
+                str.setCharAt(p2--, '0');
+                str.setCharAt(p2--, '2');
+                str.setCharAt(p2--, '%');
+            } else {
+                str.setCharAt(p2--, c);
+            }
+        }
+        return str.toString();
     }
 
     public static void main(String[] args) {
         StringBuffer stringBuffer = new StringBuffer("We Are Happy");
-        System.out.println(replaceSpace(stringBuffer));
+        System.out.println(replaceSpace3(stringBuffer));
     }
 }
